d9c73664673c7778b83587496a7ea3ce3621e82d
MateoCifrianG <mateo.cifrian@opendeusto.es>
2024-09-20 08:37:39 +0200

    Update HolaMundo.java

--- PrubaGit/src/HolaMundo.java
+++ PrubaGit/src/HolaMundo.java
@@ -2,6 +2,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 			System.out.println("Adios Mundo");
-			System.out.println("este es otro mensaje")
+			System.out.println("este es otro mensaje");
 	}
 }

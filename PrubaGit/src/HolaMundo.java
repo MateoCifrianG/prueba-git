d3dff705adccda9e2626db912d19293f30d5251d
MateoCifrianG <mateo.cifrian@opendeusto.es>
2024-09-20 08:43:07 +0200

    nuevo mensaje

--- PrubaGit/src/HolaMundo.java
+++ PrubaGit/src/HolaMundo.java
@@ -1,12 +1,10 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Adios Mundo¿¿¿¿¿	");
-=======
+		System.out.println("Adios Mundo¿¿¿¿");
 			System.out.println("Adios Mundo");
 			System.out.println("este es otro mensaje");
->>>>>>> branch 'master' of https://github.com/MateoCifrianG/prueba-git.git
+			System.out.println("este mensaje");
 	}
 }
 	
